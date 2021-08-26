2934482dc47cb863dab04a2cfe439e6b532985e7
KingSimeone <simeoneumoh@outlook.com>
2021-08-26 12:35:16 +0100

    views setup

--- whatsappbot/bot_app/views.py
+++ whatsappbot/bot_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from twilio.twiml.messaging_response import MessagingResponse
 
-# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        # retrieve incoming message from POST request in lowercase
+        incoming_msg = request.POST['Body'].lower()
+
+        # create Twilio XML response
+        resp = MessagingResponse()
+        msg = resp.message()
