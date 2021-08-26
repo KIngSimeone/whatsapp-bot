747c87639f7258e87a0eed776d218c6d024246ec
KingSimeone <simeoneumoh@outlook.com>
2021-08-26 13:05:33 +0100

    updates

--- whatsappbot/whatsappbot/settings.py
+++ whatsappbot/whatsappbot/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from dotenv import load_dotenv
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+ENV_PATH = BASE_DIR / '.env'
+load_dotenv(ENV_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-9%h&d0)w6s(0n4kf=%pk&e1osdlw7qo4=gf*a258btbs#os))2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://9ed8-102-89-3-204.ngrok.io']
+ALLOWED_HOSTS = 'https://9ed8-102-89-3-204.ngrok.io'
 
 
 # Application definition
