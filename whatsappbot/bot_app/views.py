6c14831d35f393966db21a279f3c8556bfc719c9
KingSimeone <simeoneumoh@outlook.com>
2021-08-27 07:50:46 +0100

    updates

--- whatsappbot/bot_app/views.py
+++ whatsappbot/bot_app/views.py
@@ -1,8 +1,9 @@
 from twilio.twiml.messaging_response import MessagingResponse
 from django.http import HttpResponse
+import twilio
 
 def index(request):
-    ("orint setup")
+    print(twilio)
     if request.method == 'POST':
         # retrieve incoming message from POST request in lowercase
         incoming_msg = request.POST['Body'].lower()
@@ -12,7 +13,9 @@ def index(request):
         msg = resp.message()
 
         if incoming_msg == 'hello':
-            response = "*Hi! I am the Dyneryte Bot*"
+            response = "*Hi! Welcome to dyneryte* To get started kindly type in your phone number"
             msg.body(response)
+            phone = request.POST['Body'].lower()
+            print(phone)     
 
         return HttpResponse(str(resp))
